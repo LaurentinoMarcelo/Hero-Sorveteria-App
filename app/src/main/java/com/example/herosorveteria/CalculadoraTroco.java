694c931e61b8feb6dcf7a0b7b26b220ab25feb89
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2021-05-30 18:14:32 -0300

    Finalizado a implementação do navigation drawer

---
+++ app/src/main/java/com/example/herosorveteria/CalculadoraTroco.java
@@ -0,0 +1,14 @@
+package com.example.herosorveteria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculadoraTroco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora_troco);
+    }
+}
